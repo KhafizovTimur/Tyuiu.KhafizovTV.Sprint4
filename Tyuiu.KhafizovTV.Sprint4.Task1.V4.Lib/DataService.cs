3334d2a390917a407144e6d46b27c91817d290a2
pepe <pepe@DESKTOP-JRPKCDE>
2025-11-24 00:18:45 +0300

    Fix#0

--- Tyuiu.KhafizovTV.Sprint4.Task1.V4.Lib/DataService.cs
+++ Tyuiu.KhafizovTV.Sprint4.Task1.V4.Lib/DataService.cs
@@ -6,15 +6,15 @@ namespace Tyuiu.KhafizovTV.Sprint4.Task1.V4.Lib
     {
         public int Calculate(int[] array)
         {
-            int sum = 0;
+            int product = 1;
             for (int i = 0; i < array.Length; i++)
             {
                 if (array[i] % 2 != 0)
                 {
-                    sum *= array[i];
+                    product *= array[i];
                 }
             }
-            return sum;
+            return product;
         }
     }
 }

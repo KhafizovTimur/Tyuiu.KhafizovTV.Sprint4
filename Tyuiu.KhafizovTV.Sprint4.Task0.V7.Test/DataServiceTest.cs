36aee535c6ce88dff4653b4ac313f2f9dca849a1
pepe <pepe@DESKTOP-JRPKCDE>
2025-11-23 23:58:22 +0300

    Add project files.

---
+++ Tyuiu.KhafizovTV.Sprint4.Task0.V7.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.KhafizovTV.Sprint4.Task0.V7.Lib;
+
+namespace Tyuiu.KhafizovTV.Sprint4.Task0.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultOddArrEl()
+        {
+            DataService ds = new DataService();
+
+            int wait = 0;
+            int[] numsArray = { 9, 8, 7, 9, 5, 4, 3, 2, 3, 7 };
+            int res = ds.GetMultOddArrEl(numsArray);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}

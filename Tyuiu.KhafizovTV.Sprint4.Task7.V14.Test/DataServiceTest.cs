413c6490fab299e83360e575c1eb28b1e316751f
pepe <pepe@DESKTOP-JRPKCDE>
2025-11-24 03:06:19 +0300

    Task7

---
+++ Tyuiu.KhafizovTV.Sprint4.Task7.V14.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.KhafizovTV.Sprint4.Task7.V14.Lib;
+
+namespace Tyuiu.KhafizovTV.Sprint4.Task7.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            int n = 3;
+            int m = 4;
+            int[,] mtrx = new int[n, m];
+            string str = "458712659137";
+            int res = ds.Calculate(n, m, str);
+            int wait = 38;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
